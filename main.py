3e13a35cd4e5c4a385f14fd4cc49ffc59747074c
Saxon <saxon@sirplsmy.com>
2018-09-18 07:43:49 +0000

    Added a few message responses, and a basic framework for role assignment

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ async def on_message(message):
 	if message.author == client.user:
 		return
 
+	#basic message responses
 	if message.content.startswith('!hello'):
 		msg = 'Hello Osiris'.format(message)
 		await client.send_message(message.channel, msg)
@@ -28,6 +29,35 @@ async def on_message(message):
 	if message.content.lower().startswith('!mars'):
 		msg = "```Whether we wanted it or not, we've stepped into a war with the Cabal on Mars. So let's get to taking out their command, one by one. Valus Ta'aurc. From what I can gather he commands the Siege Dancers from an Imperial Land Tank outside of Rubicon. He's well protected, but with the right team, we can punch through those defenses, take this beast out, and break their grip on Freehold.```"
 		await client.send_message(message.channel, msg)
+
+	#messing around with role assigning
+	if message.content.startswith("!setrole"):
+		console_list = ["Playstation", "Xbox", "PC"] #available roles
+		entered_console = message.content[9:] #counting characters from your command to the console entries, IE 9 for "!setrole " including the space
+		role = discord.utils.get(message.server.roles, name=entered_console) #retrieve existing roles
+		roles = [
+			"314858732204851210", #Playstation
+			"314859151152906243", #Xbox
+			"352277227951489024", #PC
+		]
+		for r in message.author.roles:
+			if r.id in roles:
+				#yell at them if they already have a role
+				await client.send_message(message.channel, "You already have a role Guardian, maybe you need to go get yelled at by Zavala")
+				return
+		if role is None or role.name not in console_list:
+			#Complain if the role doesn't exist
+			await client.send_message(message.channel, "That console doesn't exist Guardian. Please use Playstation Xbox or PC")
+			return
+		elif role in message.author.roles:
+			#if they're already got it
+			await client.send_message(message.channel, "You already have this console, Guardian.")
+		else:
+			try:
+				await client.add_roles(message.author, role)
+				await client.send_message(message.channel, "Successfully added role {0}".format(role.name))
+			except discord.Forbidden:
+				await client.send_message(message.channel, "I don't have perms to add roles") #either bot doesn't have the "Manage roles" permission, or it's below the needed roles in the priority list
 
 @client.event
 async def on_ready():
